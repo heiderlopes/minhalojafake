34c5191de47b0cb806e9c13501d94508cfa63452
Heider Pinholi Lopes <heidertreinamentos@gmail.com>
2018-08-17 11:52:01 -0300

    Hello project

---
+++ src/main/kotlin/com/amedigital/minhalojafake/MinhalojafakeApplication.kt
@@ -0,0 +1,11 @@
+package com.amedigital.minhalojafake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinhalojafakeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MinhalojafakeApplication>(*args)
+}
---
+++ src/main/kotlin/com/amedigital/minhalojafake/controller/OrderController.kt
@@ -0,0 +1,21 @@
+package com.amedigital.minhalojafake.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.servlet.http.HttpServletResponse
+
+@RestController
+@RequestMapping("/api/order")
+class OrderController {
+
+    @PostMapping()
+    fun create(response: HttpServletResponse): String {
+        val random = Random()
+        val orderId = random.nextLong().toString()
+        response.addHeader("Location", orderId)
+        response.addHeader("X-Deep-Link", "fake://order?id=$orderId")
+        return "OK"
+    }
+}
